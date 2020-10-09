fc9a1c83d7b9255e8a333ad43753dab7ecf906f2
gangulyarin <gangulyarin@users.noreply.github.com>
2020-10-10 00:03:23 +0530

    Added Bootstrap 4 Styling

--- static/script.js
+++ static/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
     $("#send").click(function(){
         text = $("#message").val()
-        $('#chat').append(text+"<br/>");
+        $('#chat').append("<p class=\"bg-primary text-white\">"+text+"</p>");
+
         $('#message').val('');
-        //alert(text)
         $.ajax({
                 type: "POST",
                 url: "/sendChat",
                 data: {'text':text.trim()},
                 dataType: "json",
                 success: function(data){
-                    //data = JSON.stringify(data);
+
                     for(var i=0; i<data.output.generic.length; i++){
                         if(data.output.generic[i].response_type == 'text'){
-                            $('#chat').append(data.output.generic[i].text+"<br/>");
+                            $('#chat').append("<p class=\"bg-secondary text-white\">"+data.output.generic[i].text+"</p>");
                         }
                         if(data.output.generic[i].response_type == 'option'){
                             for (var j = 0; j < data.output.generic[i].options.length; j++)
-                                $("#chat").append(data.output.generic[i].options[j].label+"<br/>");
+                                $("#chat").append("<div class=\"badge\">"+data.output.generic[i].options[j].label+"</div>");
                         }
                     }
-                    //$('#chat').append(data+"<br/>");
+
 
                     document.getElementById('chat').scrollTop = document.getElementById('chat').scrollHeight;
                 }
